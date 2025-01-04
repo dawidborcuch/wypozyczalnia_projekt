59d8b54ae43784d7468043623c75d19bbd58627c
DawidBo <dawid.borcuch@gmail.com>
2025-01-04 22:22:35 +0100

    Update Flask app to fetch and return weather data
    Replaced the basic route with a new `/main-site` endpoint fetching weather data from an external API. Added CORS support and imported necessary modules for the changes. These updates enable dynamic data retrieval and a more functional backend.

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+from flask_cors import CORS
+import requests
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/main-site')
+def main_site():
+    url = 'https://dobrapogoda24.pl/api/v1/weather/simple?city=warszawa&day=1&token=d21cbfc6141eaa3c1b71'
+    response = requests.get(url)
+    data = response.json()
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
